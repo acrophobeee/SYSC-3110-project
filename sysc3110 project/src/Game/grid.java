b66e6b2a3950ac1bc973783cc9a404b8c67b13ce
Jacky Chiu <jackyzhanchiu@gmail.com>
2018-10-23 18:55:23 -0400

    Add gitignore

--- sysc3110 project/src/Game/grid.java
+++ sysc3110 project/src/Game/grid.java
@@ -4,13 +4,13 @@ import java.util.Random;
 import Model.*;
 
 public class grid {
-	
+
 	private model[][] map;
-	
+
 	public grid() {
 		map = new model[5][10];
 	}
-	
+
 	public void clearMap() {
 		for (int i =0; i<5; i++) {
 			for (int j = 0; j<10; j++) {
@@ -18,21 +18,21 @@ public class grid {
 			}
 		}
 	}
-	
+
 	public void addModel(model model,int x, int y) {
 		map[x][y] = model;
 	}
-	
+
 	public void removeModel(model model, int x, int y) {
 		map[x][y] = null;
 	}
-	
+
 	public void spawnZombie() {
 		int r = (new Random()).nextInt(4)+1;
 		fastZombies fz = new fastZombies();
 		addModel(fz, 9, r);
 	}
-	
+
 	public boolean isGameOver() {
 		for (int i =0 ; i<5; i++) {
 			if (map[i][0] instanceof abstractZombies) {
@@ -41,7 +41,7 @@ public class grid {
 		}
 		return false;
 	}
-	
+
 	public void updateGrid() {
 		if (!isGameOver()) {
 			for(int i = 0; i<5; i++) {
