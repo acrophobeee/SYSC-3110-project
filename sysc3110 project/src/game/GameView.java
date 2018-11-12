7b26e628324fa0902e6b0779567856cd98048c04
nickc <nickc@172.17.155.190>
2018-11-12 18:52:41 -0500

    add image

--- sysc3110 project/src/game/GameView.java
+++ sysc3110 project/src/game/GameView.java
@@ -17,9 +17,11 @@ public class GameView extends JFrame {
   private JPanel pvz,info;
   private JButton skip, sun, pea;
   private JTextField point;
-  private JTextField[][] gg;
+  private JButton[][] gg;
   private JLabel sp;
   private int rows, columns;
+  private ImageIcon peacho = new ImageIcon("pea.jpg");
+  private ImageIcon sunn = new ImageIcon("images.jpg");
   
   public GameView(ActionListener al, int rows, int columns) {
     super();
@@ -43,30 +45,28 @@ public class GameView extends JFrame {
     opt.add(quit);
     pvz = new JPanel(new GridLayout(rows,columns));
     info = new JPanel(new GridLayout(1,3));
-    info.setPreferredSize(new Dimension(300,175));
-    gg = new JTextField[rows][columns];
+    info.setPreferredSize(new Dimension(300,100));
+    gg = new JButton[rows][columns];
     point = new JTextField();
     point.setEditable(false);
     point.setFont(f);
     //point.setText(String.valueOf(g.getSp()));
     for (int row=0; row<rows; row++) {
       for(int column=0; column<columns; column++) {
-        gg[row][column]= new JTextField();
-        gg[row][column].setEditable(false);
-        gg[row][column].setFont(f);
+        gg[row][column]= new JButton();
+        gg[row][column].setEnabled(false);
         gg[row][column].setBackground(new Color(124,252,0));
         pvz.add(gg[row][column]);
       }
     }
-    ImageIcon peacho = new ImageIcon("pea.jpg");
-    ImageIcon sunn = new ImageIcon("images.jpg");
     skip = new JButton("skip");
     skip.addActionListener(al);
     skip.setFont(f);
-    sun = new JButton("sun", sunn );
+    sun = new JButton("sun",sunn);
     sun.addActionListener(al);
     sun.setFont(f);
     pea = new JButton("pea", peacho);
+    pea.setFont(f);
     pea.addActionListener(al);
     sp = new JLabel();
     sp.setText("Sun points: ");
@@ -84,7 +84,7 @@ public class GameView extends JFrame {
     this.getContentPane().add(info,BorderLayout.AFTER_LAST_LINE);
 
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setSize(1000, 1000);
+    this.setSize(1100, 800);
   }
 
   public JMenu getOpt() {
@@ -155,20 +155,12 @@ public class GameView extends JFrame {
     return point;
   }
 
-  public void setPoint(JTextField point) {
-    this.point = point;
-  }
-
-  public JTextField[][] getGg() {
+  public JButton[][] getGg() {
     return gg;
   }
 
   public JLabel getSp() {
     return sp;
-  }
-
-  public void setSp(JLabel sp) {
-    this.sp = sp;
   }
 
   public void renderGrid(Grid grid) {
@@ -184,9 +176,13 @@ public class GameView extends JFrame {
         Model m = grid.getModel(row, column);
         if (m == null) {
           this.gg[row][column].setText("");
-        } else {
-          this.gg[row][column].setText(m.getType().toString());
+        } 
+        else if (m instanceof PeaShooter){
+          this.gg[row][column].setIcon(peacho);
         }
+        else if (m instanceof SunFlower){
+            this.gg[row][column].setIcon(sunn);
+          }
       }
     }
   }
