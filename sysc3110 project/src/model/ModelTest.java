1e13034cb0ea8957565edb189853a483f43c03de
kirinrastogi <rastogikirin@gmail.com>
2018-11-12 21:57:36 -0500

    add tests for model

---
+++ sysc3110 project/src/model/ModelTest.java
@@ -0,0 +1,65 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ModelTest {
+	Model m;
+
+	@Before
+	public void setUp() throws Exception {
+		m = new SunFlower();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetType() {
+		assert (m.getType() == ModelType.SUN_FLOWER);
+	}
+
+	@Test
+	public void testSetType() {
+		m.setType(ModelType.BULLET);
+		assert (m.getType() == ModelType.BULLET);
+	}
+
+	@Test
+	public void testGetHp() {
+		assert (m.getHp() == 50);
+	}
+
+	@Test
+	public void testSetHp() {
+		m.setHp(69);
+		assert (m.getHp() == 69);
+	}
+
+	@Test
+	public void testGetAtk() {
+		assert (m.getAtk() == 0);
+	}
+
+	@Test
+	public void testSetAtk() {
+		m.setAtk(69);
+		assert (m.getAtk() == 69);
+	}
+
+	@Test
+	public void testGetCd() {
+		assert (m.getCd() == 3);
+	}
+
+	@Test
+	public void testSetCd() {
+		m.setCd(4);
+		assert (m.getCd() == 4);
+	}
+
+}
